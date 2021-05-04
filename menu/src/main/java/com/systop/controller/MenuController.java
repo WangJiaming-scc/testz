cff5efa78a9d90d2d5d810f8ef622858a2eda9b0
WangJiaming-scc <1756585149@qq.com>
2021-05-04 19:22:13 +0800

    更新menu中的findAll方法

--- menu/src/main/java/com/systop/controller/MenuController.java
+++ menu/src/main/java/com/systop/controller/MenuController.java
@@ -1,13 +1,22 @@
 package com.systop.controller;
 
+import com.systop.entity.Menu;
+import com.systop.repository.MenuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/menu")
 public class MenuController {
+
+    @Autowired
+    private MenuRepository menuRepository;
 
     @Value("${server.port}")
     private String port;
@@ -15,5 +24,10 @@ public class MenuController {
     @GetMapping("/index")
     public String index(){
         return "menu的当前端口为："+this.port;
+    }
+
+    @GetMapping("/findAll/{index}/{limit}")
+    public List<Menu> findAll(@PathVariable("index") int index,@PathVariable("limit") int limit){
+        return menuRepository.findAll(index, limit);
     }
 }
